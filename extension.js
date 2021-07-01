22bfc6ec7b6538b6bf28bb5e36c364b96ecde45e
harshagl2002 <79414830+harshagl2002@users.noreply.github.com>
2021-07-01 17:13:48 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 function getCourseInfo(searchText) {
     const regexNumbers = /[0-9]+/
-    const name = searchText.split(regexNumbers)[0].toUpperCase().trim();
+    const name = searchText.split(regexNumbers)[0].toUpperCase().trim().toString();
     const possibleNumbers = searchText.match(regexNumbers);
     let number = null;
     if (possibleNumbers !== null) {
-        number = possibleNumbers[0].trim();
+        number = possibleNumbers[0].trim().toString();
     }
     return [name, number];
 }
@@ -33,10 +33,6 @@ function getCourseTermInfo(searchText) {
 }
 
 function getCourseTermNumInfo(searchText) {
-    /*const term = arr[0];
-    const year = arr[1];
-    const course = arr[2];
-    const number = arr[3];*/
     const regexNumbers = /[0-9]+/
     const term = searchText.split(regexNumbers)[0].toLowerCase().trim().toString();
     const course = searchText.split(regexNumbers)[1].toUpperCase().trim().toString();
@@ -67,76 +63,60 @@ function addLocations(rows) {
 const rows = document.getElementsByTagName("tr");
 addLocations(rows);
 
-const searchForm = document.getElementById("subjectAutoJump-form");
-searchForm.addEventListener("submit", (event) => {
-    console.log("Got a submit henlo");
-    const searchBox = document.getElementById("subjectAutoJump");
-    const searchText = searchBox.value;
-    console.log(searchText);
-
-    const [name, number] = getCourseInfo(searchText);
-    console.log(`${name}-${number}`);
-    if (number == null) {
-        return;
-    }
-
-    const newURL = `https://courses.illinois.edu/schedule/DEFAULT/DEFAULT/${name}/${number}`;
-    window.location.href = newURL;
-    event.preventDefault();
-});
-
 
 const searchEngine = document.getElementById("subjectAutoJump-form");
 searchEngine.addEventListener("submit", (event) => {
-    console.log("Got a submit henlo");
     const searchBox = document.getElementById("subjectAutoJump");
-    const searchText = searchBox.value;
-    console.log(searchText);
+    const searchText = searchBox.value.toString();
+    const searchTextArr = searchText.split(" ");
+    if (searchTextArr.length == 2) {
+        const [name, number] = getCourseInfo(searchText);
+        if (name.localeCompare("FALL") == 0 || name.localeCompare("SPRING") == 0 || name.localeCompare("SUMMER") == 0) {
+            var d = new Date();
+            var currentYear = d.getFullYear().toString();
+            const [term, year] = getTermInfo(searchText);
 
-    const [term, year] = getTermInfo(searchText);
-    console.log(`${term}-${year}`);
-    var d = new Date();
-    var currentYear = d.getFullYear().toString();
-    if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear) > 0) {
-        return;
-    }
-    const newLoc = `https://courses.illinois.edu/schedule/${year}/${term}`;
-    window.location.href = newLoc;
-    event.preventDefault();
-});
+           if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear) > 0) {
+                return;
+            } else {
 
-const searchBar = document.getElementById("subjectAutoJump-form");
-searchBar.addEventListener("submit", (event) => {
-    const searchBox = document.getElementById("subjectAutoJump");
-    const searchText = searchBox.value;
-    const [term, year, course] = getCourseTermInfo(searchText);
+                const newLoc = `https://courses.illinois.edu/schedule/${year}/${term}`;
+                window.location.href = newLoc;
+                event.preventDefault();
+            }
+        } else {
+            const newURL = `https://courses.illinois.edu/schedule/DEFAULT/DEFAULT/${name}/${number}`;
+            window.location.href = newURL;
+            event.preventDefault();
+        }
+    } else if (searchTextArr.length == 3) {
+            const [term, year, course] = getCourseTermInfo(searchText);
 
-    var d = new Date();
-    var currentYear = d.getFullYear().toString();
+        var date2 = new Date();
+        var currentYear3 = date2.getFullYear().toString();
 
-    if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear) > 0) {
-        return;
-    }
-    const newURL = `https://courses.illinois.edu/schedule/${year}/${term}/${course}`;
+        if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear3) > 0) {
+            return;
+        }
+        const newURL = `https://courses.illinois.edu/schedule/${year}/${term}/${course}`;
         window.location.href = newURL;
-    event.preventDefault();
-});
+        event.preventDefault();
+    } else if (searchTextArr.length == 4) {
+        const [term, year, course, number] = getCourseTermNumInfo(searchText);
 
-const searchMenu = document.getElementById("subjectAutoJump-form");
-searchMenu.addEventListener("submit", (event) => {
-    const searchBox = document.getElementById("subjectAutoJump");
-    const searchText = searchBox.value;
-    const [term, year, course, number] = getCourseTermNumInfo(searchText);
+        var date = new Date();
+        var currentYear2 = date.getFullYear().toString();
 
-    var d = new Date();
-    var currentYear = d.getFullYear().toString();
-
-    if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear) > 0) {
-        return;
+        if (year.localeCompare("2004") < 0 || year.localeCompare(currentYear2) > 0) {
+            return;
+        }
+        const newURL = `https://courses.illinois.edu/schedule/${year}/${term}/${course}/${number}`;
+        window.location.href = newURL;
+        event.preventDefault();
     }
-    const newURL = `https://courses.illinois.edu/schedule/${year}/${term}/${course}/${number}`;
-    window.location.href = newURL;
-    event.preventDefault();
 });
+
+
+
 
 
